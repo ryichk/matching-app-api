1e73e86cd2b66dff5f32314354e275a39f632bb6
ryichk <ryichk@gmail.com>
2021-06-30 02:11:08 +0900

    added user model spec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,65 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'creation successful' do
+    context 'valid values' do
+      it 'is correct name, email, password and password_confirmation' do
+        user = build(:user)
+        expect(user).to be_valid
+      end
+
+      it 'is password length of 8 characters' do
+        user = build(:user, password: 'password', password_confirmation: 'password')
+        expect(user).to be_valid
+      end
+
+      it 'is password length of 32 characters' do
+        user = build(:user, password: 'passwordpasswordpasswordpassword', password_confirmation: 'passwordpasswordpasswordpassword')
+        expect(user).to be_valid
+      end
+    end
+  end
+
+  describe 'creation failure' do
+    context 'blank value' do
+      it 'is blank name' do
+        user = build(:user, name: nil)
+        expect(user).to be_invalid
+      end
+
+      it 'is blank email' do
+        user = build(:user, email: nil)
+        expect(user).to be_invalid
+      end
+
+      it 'is blank password' do
+        user = build(:user, password: nil)
+        expect(user).to be_invalid
+      end
+    end
+
+    context 'invalid value' do
+      it 'is password length of 7 characters' do
+        user = build(:user, password: 'passwor', password_confirmation: 'passwor')
+        expect(user).to be_invalid
+      end
+
+      it 'is password length of 33 characters' do
+        user = build(:user, password: 'passwordpasswordpasswordpasswordp', password_confirmation: 'passwordpasswordpasswordpasswordp')
+        expect(user).to be_invalid
+      end
+
+      it 'is registered email' do
+        create(:user, email: 'test@example.com')
+        user = build(:user, email: 'test@example.com')
+        expect(user).to be_invalid
+      end
+
+      it 'is different password and password_confirmation' do
+        user = build(:user, password: 'password', password_confirmation: 'diffpassword')
+        expect(user).to be_invalid
+      end
+    end
+  end
+
+end
