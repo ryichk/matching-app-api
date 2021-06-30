d3fb47fb52f3154f430bf0beea64db9eb9b64dfc
ryichk <ryichk@gmail.com>
2021-06-30 23:19:19 +0900

    make factory user name unique

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :user do
-    name {'test'}
-    sequence(:email) { |n| "test#{n}@example.com"}
-    password {'password'}
-    password_confirmation {'password'}
+    sequence(:name) { |n| "test#{n}" }
+    sequence(:email) { |n| "test#{n}@example.com" }
+    password { 'password' }
+    password_confirmation { 'password' }
   end
 end
